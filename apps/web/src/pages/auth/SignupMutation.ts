3ff89b28cbf51a359602012be9423773563c16e5
Thiago <lucimara1422@gmail.com>
2023-12-22 19:55:51 -0300

    feat(auth) login component

---
+++ apps/web/src/pages/auth/SignupMutation.ts
@@ -0,0 +1,21 @@
+import { graphql } from "react-relay";
+
+const SignUpMutation = graphql`
+  mutation SignupMutation(
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    userRegisterMutation(
+      input: { username: $username, password: $password, email: $email }
+    ) {
+      token
+      me {
+        id
+        username
+      }
+    }
+  }
+`;
+
+export { SignUpMutation };
--- apps/web/src/pages/auth/signin.tsx
+++ apps/web/src/pages/auth/signin.tsx
@@ -1,76 +1,101 @@
-import { z } from 'zod'
-import { useForm } from 'react-hook-form'
-import { useContext } from 'react'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useMutation } from 'react-relay'
+import Image from "next/image";
+import type { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
+import { useState } from "react";
+import { useMutation } from "react-relay";
+import { useRouter } from "next/navigation";
+import { Input } from "../../../../../packages/ui/src/input";
+import { Icons } from "../../../../../packages/ui/src/icons";
+import { UserAuth } from "../../context/AuthContext";
+import { toast } from "../../../../../packages/ui/src/useToast";
+import { SignInMutation } from "./SigninMutation";
 
-import { GetServerSideProps } from 'next'
-import { parseCookies } from 'nookies'
-import { AuthContext } from '../../context/AuthContext'
+export default function Signin() {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const { signIn } = UserAuth();
+  const [submit] = useMutation(SignInMutation);
 
-import { SignInMutation } from '../../context/user/SigninMutation'
-import { useToast } from '../../hooks/useToast'
+  const handleLogin = ({ email, password }) => {
+    setIsLoading(true);
+    submit({
+      variables: {
+        email,
+        password,
+      },
+      onError(error) {
+        console.log(error);
+      },
+      onCompleted({ userLoginMutation }) {
+        const token = userLoginMutation?.token ?? "";
+        const username = userLoginMutation?.me?.username;
 
+        toast({
+          title: `Welcome ${username} 🚀`,
+          description: "Welcome to gameles!!!",
+        });
 
-export default function SignIn() {
-  // const { toast } = useToast()
-  // const { signIn } = useContext(AuthContext)
- 
-  // const [submit] = useMutation<SigninMutation>(SignInMutation)
+        signIn(token);
+        router.push("/");
+        setIsLoading(false);
+      },
+    });
+  };
 
-  // function handleLogin({ email, password }: UserLoginFormData) {
-  //   submit({
-  //     variables: {
-  //       email,
-  //       password,
-  //     },
-  //     onError(error) {
-  //       if (error.name === 'TypeError') {
-  //         toast({
-  //           variant: 'destructive',
-  //           title: 'Uh oh! Something went wrong',
-  //           description: 'connection failed',
-  //         })
-  //       } else {
-  //         toast({
-  //           variant: 'destructive',
-  //           title: 'Uh oh! Something went wrong',
-  //           description: 'Login failed',
-  //         })
-  //       }
-  //     },
-  //     onCompleted({ userLoginMutation }: SigninMutation$data) {
-  //       const token = userLoginMutation?.token ?? ''
-  //       const username = userLoginMutation?.me?.username
-
-  //       toast({
-  //         title: `Welcome ${username} 🚀`,
-  //         description: 'Make an appointment now!',
-  //       })
-
-  //       signIn(token)
-  //       router.push('/')
-  //     },
-  //   })
-  // }
   return (
-   <h1>signin</h1>
-  )
+    <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative hidden h-full flex-col bg-[url('/home.svg')] bg-auto bg-no-repeat bg-center p-10 text-white dark:border-r lg:flex">
+        <div className="absolute inset-0 " />
+      </div>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <div className="flex justify-center mx-auto">
+              <Image
+                alt="logo pix em cima da mão"
+                height={70}
+                priority
+                src="/pix-hand.svg"
+                width={70}
+              />
+            </div>
+            <p className="mt-3 font-bold text-xl">
+              Crie uma conta <br /> para o seu comércio
+            </p>
+            <p className="text-gray-500 mt-4">entre com sua conta Google.</p>
+          </div>
+          <Input name="email" placeholder="email" />
+          <Input name="password" placeholder="password" type="password" />
+          <button
+            className="bg-gray-800 hover:bg-gray-900 transition-all"
+            disabled={isLoading}
+            onClick={() => {
+              handleLogin({ email: "thiago@email.com", password: "123456" });
+            }}
+            type="button"
+          >
+            <Icons.Spinner className="mr-2 h-4 w-4 animate-spin" />
+            Entrar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { 'game-token': token } = parseCookies(ctx)
+  const { "game-token": token } = parseCookies(ctx);
 
   if (token) {
     return {
       redirect: {
-        destination: '/',
+        destination: "/",
         permanent: false,
       },
-    }
+    };
   }
 
   return {
     props: {},
-  }
-}
+  };
+};
