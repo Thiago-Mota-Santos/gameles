432c7cc48f18ab2b48f5dade9500a6d6f7581ee6
Thiago <lucimara1422@gmail.com>
2023-12-22 20:08:22 -0300

    fix(eslint) pascal case instead small case

--- packages/eslint-config-custom/react.js
+++ packages/eslint-config-custom/react.js
@@ -37,6 +37,7 @@ module.exports = {
   // add rules configurations here
   rules: {
     "import/no-default-export": "off",
-    "@typescript-eslint/explicit-function-return-type": "off"
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "unicorn/filename-case": "off"
   },
 };
