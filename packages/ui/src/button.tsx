556500981d176b4beb9d47b5cf268db5ef4dcae4
Thiago <lucimara1422@gmail.com>
2023-12-22 18:51:32 -0300

    feat(UI) toast

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -40,14 +40,16 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, children, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {children}
+      </Comp>
     )
   },
 )
