8f396a0228259dc6d5da020372c2163df9c9a718
Thiago <lucimara1422@gmail.com>
2023-12-28 18:41:21 -0300

    feat(auth) context

--- apps/web/src/context/AuthContext.tsx
+++ apps/web/src/context/AuthContext.tsx
@@ -1,34 +1,33 @@
-import { Maybe } from '@/interfaces/Maybe'
-import { destroyCookie, setCookie } from 'nookies'
-import { getAuthToken } from '@/utils/getToken'
-
-import React, { useState, useMemo, useCallback } from 'react'
+import { destroyCookie, setCookie } from "nookies";
+import React, { useState, useMemo, useCallback, useContext } from "react";
+import { Maybe } from "@repo/types";
+import { getAuthToken } from "@/utils/getToken";
 
 interface AuthContextValue {
-  token: Maybe<string>
-  signIn: (token: Maybe<string>) => void
-  signout: () => void
+  token: Maybe<string>;
+  signIn: (token: Maybe<string>) => void;
+  signout: () => void;
 }
 
-const AUTH_COOKIE = 'game-token'
-export const AuthContext = React.createContext({} as AuthContextValue)
+const AUTH_COOKIE = "game-token";
+export const AuthContext = React.createContext({} as AuthContextValue);
 
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [userToken, setUserToken] = useState<AuthContextValue['token']>(() =>
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [userToken, setUserToken] = useState<AuthContextValue["token"]>(() =>
     getAuthToken(),
-  )
-  const signIn = useCallback<AuthContextValue['signIn']>((token) => {
+  );
+  const signIn = useCallback<AuthContextValue["signIn"]>((token) => {
     setCookie(undefined, AUTH_COOKIE, token, {
       maxAge: 3600 * 24 * 7,
-      path: '/',
-    })
-    setUserToken(token)
-  }, [])
+      path: "/",
+    });
+    setUserToken(token);
+  }, []);
 
-  const signout = useCallback<AuthContextValue['signout']>(() => {
-    setUserToken(null)
-    destroyCookie(undefined, AUTH_COOKIE)
-  }, [])
+  const signout = useCallback<AuthContextValue["signout"]>(() => {
+    setUserToken(null);
+    destroyCookie(undefined, AUTH_COOKIE);
+  }, []);
 
   const value = useMemo<AuthContextValue>(
     () => ({
@@ -37,7 +36,11 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       signout,
     }),
     [userToken, signIn, signout],
-  )
+  );
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function UserAuth() {
+  return useContext(AuthContext);
 }
