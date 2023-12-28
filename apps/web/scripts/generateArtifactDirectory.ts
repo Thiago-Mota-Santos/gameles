3cb4077759ee3d09ed3e5c1ffdd206a2817b9e36
Thiago <lucimara1422@gmail.com>
2023-12-28 18:56:28 -0300

    feat(relay) script front

--- apps/web/scripts/generateArtifactDirectory.ts
+++ apps/web/scripts/generateArtifactDirectory.ts
@@ -1,24 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const fs = require('fs')
+const fs = require("node:fs");
 
-const dir = '__generated__'
+const dir = "__generated__";
 
 const relayPackages = [
   {
-    name: 'web',
+    name: "web",
     path: `./src/${dir}`,
   },
-]
+];
 
-;(async () => {
+void (async () => {
   relayPackages.map((package) => {
     if (!fs.existsSync(package.path)) {
-      console.log(`__generated__ folder created on ${package.name}`)
-      fs.mkdirSync(package.path)
+      console.log(`__generated__ folder created on ${package.name}`);
+      fs.mkdirSync(package.path);
     } else {
-      console.log(`__generated__ folder exists on ${package.name}`)
+      console.log(`__generated__ folder exists on ${package.name}`);
     }
-  })
+  });
 
-  process.exit(0)
-})()
+  process.exit(0);
+})();
