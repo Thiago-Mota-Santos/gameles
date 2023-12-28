4d3cee185e7ae9a6c4206d8d3e41d60432dcbb1b
Thiago <lucimara1422@gmail.com>
2023-12-28 19:02:02 -0300

    feat(next) nextconfig

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@repo/ui"],
-}
+  transpilePackages: ["@repo/ui", "@repo/types"],
+  images: {
+    domains: ["plus.unsplash.com"],
+  },
+};
 
-
-module.exports = nextConfig
-
-
+module.exports = nextConfig;
