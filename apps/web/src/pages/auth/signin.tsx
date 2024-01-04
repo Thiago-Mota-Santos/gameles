25477a7a264965b1aa6c47ab112f1cb6ed9a1b0a
Thiago <lucimara1422@gmail.com>
2024-01-04 17:50:37 -0300

    feat(signin) page

--- apps/web/src/pages/auth/signin.tsx
+++ apps/web/src/pages/auth/signin.tsx
@@ -1,37 +1,54 @@
 import Image from "next/image";
 import type { GetServerSideProps } from "next";
 import { parseCookies } from "nookies";
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import { useMutation } from "react-relay";
 import { useRouter } from "next/navigation";
 import { Input } from "@repo/ui/input";
 import { Icons } from "@repo/ui/icons";
-import { toast } from "@repo/ui/useToast";
+import { Button } from "@repo/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormSubmit,
+  toast,
+} from "@repo/ui/index";
 import { UserAuth } from "../../context/AuthContext";
 import { SignInMutation } from "./SigninMutation";
+import { SigninMutation$data } from "@/__generated__/SigninMutation.graphql";
+
+interface Data {
+  email?: string;
+  password?: string;
+}
 
 export default function Signin() {
   const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<Data>({
+    email: "",
+    password: "",
+  });
   const router = useRouter();
   const { signIn } = UserAuth();
   const [submit] = useMutation(SignInMutation);
 
-  const handleLogin = ({ email, password }) => {
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     setIsLoading(true);
     submit({
       variables: {
-        email,
-        password,
+        email: data.email,
+        password: data.password,
       },
       onError(error) {
         console.log(error);
       },
-      onCompleted({ userLoginMutation }) {
+      onCompleted({ userLoginMutation }: SigninMutation$data) {
         const token = userLoginMutation?.token ?? "";
         const username = userLoginMutation?.me?.username;
 
-        toast({
-          title: `Welcome ${username} 🚀`,
+        toast(`Welcome ${username}`, {
           description: "Welcome to gameles!!!",
         });
 
@@ -43,43 +60,79 @@ export default function Signin() {
   };
 
   return (
-    <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <div className="relative hidden h-full flex-col bg-[url('/home.svg')] bg-auto bg-no-repeat bg-center p-10 text-white dark:border-r lg:flex">
-        <div className="absolute inset-0 " />
-      </div>
-      <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <div className="flex justify-center mx-auto">
-              <Image
-                alt="logo pix em cima da mão"
-                height={70}
-                priority
-                src="/pix-hand.svg"
-                width={70}
-              />
+    <Form onSubmit={handleLogin}>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-[url('/home.svg')] bg-auto bg-no-repeat bg-center p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 " />
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <div className="flex justify-center mx-auto">
+                <Image
+                  alt="game icon"
+                  height={70}
+                  priority
+                  src="/game.svg"
+                  width={70}
+                />
+              </div>
+              <p className="mt-3 font-bold text-xl">Create an account</p>
+              <p className="text-gray-500 mt-4">
+                Talk about games with your friends
+              </p>
             </div>
-            <p className="mt-3 font-bold text-xl">
-              Crie uma conta <br /> para o seu comércio
-            </p>
-            <p className="text-gray-500 mt-4">entre com sua conta Google.</p>
+
+            <FormField name="email">
+              <FormControl asChild>
+                <Input
+                  name="email"
+                  onChange={(e) => {
+                    setData((prevData) => ({
+                      ...prevData,
+                      email: e.target.value,
+                    }));
+                  }}
+                  placeholder="Digit your email"
+                  type="email"
+                  value={data.email}
+                />
+              </FormControl>
+            </FormField>
+
+            <FormField name="password">
+              <FormControl asChild>
+                <Input
+                  name="password"
+                  onChange={(e) => {
+                    setData((prevData) => ({
+                      ...prevData,
+                      password: e.target.value,
+                    }));
+                  }}
+                  placeholder="password"
+                  type="password"
+                  value={data.password}
+                />
+              </FormControl>
+            </FormField>
+
+            <FormSubmit asChild>
+              <Button
+                className="max-w-[240px] flex items-center justify-center px-6 py-3 mt-4 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 hover:bg-gray-50 w-full md:max-w-none"
+                disabled={isLoading}
+                variant="ghost"
+              >
+                {isLoading ? (
+                  <Icons.Spinner className="mr-2 h-4 w-4 animate-spin" />
+                ) : null}
+                <span className="mx-2">Entrar</span>
+              </Button>
+            </FormSubmit>
           </div>
-          <Input name="email" placeholder="email" />
-          <Input name="password" placeholder="password" type="password" />
-          <button
-            className="bg-gray-800 hover:bg-gray-900 transition-all"
-            disabled={isLoading}
-            onClick={() => {
-              handleLogin({ email: "thiago@email.com", password: "123456" });
-            }}
-            type="button"
-          >
-            <Icons.Spinner className="mr-2 h-4 w-4 animate-spin" />
-            Entrar
-          </button>
         </div>
       </div>
-    </div>
+    </Form>
   );
 }
 
